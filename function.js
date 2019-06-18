2f238c1d612b977ced0b41c5a86ca7dbe9861f79
Nikita <nikit-kurochkin@yandex.ru>
2019-06-18 11:50:04 +0700

    new

---
+++ function.js
@@ -0,0 +1,29 @@
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+// showFirstMessage("Hello World");
+// console.log(num);
+
+// let calc = function calc(a,b) {
+//     return (a+b);
+// }
+
+let calc = (a,b) => (a + b)
+
+console.log(calc(3,4));
+
+console.log(calc(8,4));
+
+
+function retVar() {
+    let num = 50;
+    return num;
+
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
