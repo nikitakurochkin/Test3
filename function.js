1116bdc09d0b120c665b5e74402688dcf4831d9d
Nikita <nikit-kurochkin@yandex.ru>
2019-06-18 12:11:30 +0700

    new function

--- function.js
+++ function.js
@@ -27,3 +27,17 @@ function retVar() {
 
 let anotherNum = retVar();
 console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+
+// console.log(Math.round(twelve));
+
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
+
